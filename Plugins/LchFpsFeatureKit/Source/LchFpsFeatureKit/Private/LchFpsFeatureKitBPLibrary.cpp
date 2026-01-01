42f27b25cdab2137f81f08f537c4903018af1d3f
cwl\chenweilin <weilin1013@qq.com>
2026-01-01 17:48:46 +0800

    plugins漏了

---
+++ Plugins/LchFpsFeatureKit/Source/LchFpsFeatureKit/Private/LchFpsFeatureKitBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LchFpsFeatureKitBPLibrary.h"
+#include "LchFpsFeatureKit.h"
+
+ULchFpsFeatureKitBPLibrary::ULchFpsFeatureKitBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float ULchFpsFeatureKitBPLibrary::LchFpsFeatureKitSampleFunction(float Param)
+{
+	return -1;
+}
+
